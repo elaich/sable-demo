b7bbfb43b0d2590b8324406023e1bc5543d1610c
Marouane El Aich <elaich.marouane@gmail.com>
2019-09-22 19:20:58 +0100

    Complete data source and model

--- server/src/suspicious/models/transaction.ts
+++ server/src/suspicious/models/transaction.ts
@@ -1,7 +1,30 @@
-import { Field, ID, ObjectType } from 'type-graphql';
+import { Field, ID, Int, ObjectType, registerEnumType } from 'type-graphql';
+
+export enum TransactionState {
+  Suspicious,
+  Approved,
+  Declined
+}
+
+registerEnumType(TransactionState, {
+  name: "TransactionState",
+});
 
 @ObjectType()
 export class Transaction {
   @Field(type => ID)
   id: string;
+
+  @Field(type => ID)
+  from: string;
+
+
+  @Field(type => ID)
+  to: string;
+
+  @Field(type => Int)
+  amount: number;
+
+  @Field(type => TransactionState)
+  state: TransactionState;
 }
--- server/src/suspicious/transactions.service.ts
+++ server/src/suspicious/transactions.service.ts
@@ -1,10 +1,48 @@
 import { Injectable } from '@nestjs/common';
-import { Transaction } from './models/transaction';
+import { Transaction, TransactionState } from './models/transaction';
 
 @Injectable()
 export class TransactionsService {
 
+  private readonly transactions: Transaction[] = [
+    {
+      id: '1',
+      from: 'a3ad',
+      to: 'h4n2',
+      amount: 309,
+      state: TransactionState.Suspicious,
+    },
+    {
+      id: '2',
+      from: 'h7s9',
+      to: 'ys3m',
+      state: TransactionState.Suspicious,
+      amount: 30
+    },
+    {
+      id: '3',
+      from: 'ju87',
+      to: 'hd63',
+      state: TransactionState.Suspicious,
+      amount: 139
+    },
+    {
+      id: '4',
+      from: 'hod2',
+      to: '6dk3',
+      state: TransactionState.Suspicious,
+      amount: 200
+    },
+    {
+      id: '5',
+      from: '8dk2',
+      to: 'lk82',
+      state: TransactionState.Suspicious,
+      amount: 90
+    },
+  ]
+
   findSuspicious(): Transaction[] {
-    return [];
+    return this.transactions;
   }
 }
